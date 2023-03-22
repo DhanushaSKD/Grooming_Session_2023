311eeef15188be9a117b1f92e31660cddf68834a
dhad <dhanusha.d@publicissapient.com>
2023-03-22 20:03:07 +0530

    program 1 completed

---
+++ demo/src/main/java/com/dhanusha/Sum.java
@@ -0,0 +1,13 @@
+//Java program to accept two numbers as command line arguments and print the addition of those numbers.
+
+package com.dhanusha;
+
+public class Sum 
+{
+    public static void main( String[] args )
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum of a and b is "+(a+b));
+    }
+}
